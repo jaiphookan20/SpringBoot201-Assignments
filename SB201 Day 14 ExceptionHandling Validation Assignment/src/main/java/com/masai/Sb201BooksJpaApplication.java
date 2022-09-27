d1c434e38d03cd3d95cd54321eb9eacc64e0d6f3
Jai Phookan <52240311+jaiphookan20@users.noreply.github.com>
2022-09-27 18:12:17 +0530

    Added assignment files

---
+++ SB201 Day 14 ExceptionHandling Validation Assignment/src/main/java/com/masai/Sb201BooksJpaApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb201BooksJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb201BooksJpaApplication.class, args);
+	}
+
+}
